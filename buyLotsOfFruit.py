8b7fe79b75c7cc4ab9452fbec0214651d6de9647
pramodC137 <pramodmadhushan4@gmail.com>
2026-09-10 23:36:55 +0530

    Section 4 complete

--- buyLotsOfFruit.py
+++ buyLotsOfFruit.py
@@ -35,7 +35,11 @@ def buyLotsOfFruit(orderList):
     Returns cost of order
     """
     totalCost = 0.0
-    "*** YOUR CODE HERE ***"
+    for fruit, numPounds in orderList:
+        if fruit not in fruitPrices:
+            print("Error: %s does not exist in the price list. Order cannot be priced." % fruit)
+            return None
+        totalCost += fruitPrices[fruit] * numPounds
     return totalCost
 
 
